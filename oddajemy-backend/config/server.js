e48d66547e753ee2106f266d8bf66526642867a7
Kamil <kamil@rebornlab.com>
2019-10-22 15:33:47 +0200

    hello world

--- oddajemy-backend/config/server.js
+++ oddajemy-backend/config/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import db from './dbConnection';
 import middleware from './middleware';
-import routes from './routes';
+// import routes from './routes';
 
 const app = express();
 
@@ -14,7 +14,14 @@ db.connect((err) => {
 
 middleware(app);
 
-routes(app);
+// routes(app);
+app.get('/users', (req, res) => {
+    db.query('SELECT * FROM users', (err, result) => {
+      if(err) throw err;
+      console.log(result);
+      res.send('Fetched data...');
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 
